76fef38dac5e80d7ee857f225a9aafd6db49b1ed
Yuuki Fujii <brave7822@gmail.com>
2019-07-31 04:46:00 +0000

    login,logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
   root to: 'pages#home'
   get 'pages/home'
+  get "pages/mypage" => "pages#mypage"
   
   resource :users
   
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
