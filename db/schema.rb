4ce3bd7ee4e53be0e4bb7095c35e4b7935be0b37
Yuuki Fujii <brave7822@gmail.com>
2019-08-08 01:31:35 +0000

    user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_003359) do
+ActiveRecord::Schema.define(version: 2019_08_06_073813) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
