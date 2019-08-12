1cc163c3bc3fea3155d0dacfd5a95bc4a87bb81c
Yuuki Fujii <brave7822@gmail.com>
2019-08-12 12:08:25 +0000

    exercise index

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,14 +2,21 @@ class ExercisesController < ApplicationController
   
   def new
     @exercise = Exercise.new
-    @exercises = Exercise.all
+    @exercises1 = Exercise.where(body_id: 1)
+    @exercises2 = Exercise.where(body_id: 2)
+    @exercises3 = Exercise.where(body_id: 3)
+    @exercises4 = Exercise.where(body_id: 4)
+    @exercises5 = Exercise.where(body_id: 5)
+    @exercises6 = Exercise.where(body_id: 6)
+    @exercises7 = Exercise.where(body_id: 7)
+    @exercises8 = Exercise.where(body_id: 8)
   end
   
   def create
     @exercise = Exercise.new(exercise_params)
     
     if @exercise.save
-      redirect_to pages_mypage_path, success: '新しい種目を追加しました'
+      redirect_to new_exercises_path, success: '新しい種目を追加しました'
     else
       flash.now[:danger] = "種目の追加に失敗しました"
       render :new
