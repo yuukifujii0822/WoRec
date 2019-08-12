704018b5b6797f63086a7f82143112261121d562
Yuuki Fujii <brave7822@gmail.com>
2019-08-12 10:20:00 +0000

    exercise add

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,17 +1,26 @@
 class ExercisesController < ApplicationController
+  
   def new
     @exercise = Exercise.new
+    @exercises = Exercise.all
   end
   
   def create
-    @exerise = Exercise.new(exercise_params)
+    @exercise = Exercise.new(exercise_params)
+    
+    if @exercise.save
+      redirect_to pages_mypage_path, success: '新しい種目を追加しました'
+    else
+      flash.now[:danger] = "種目の追加に失敗しました"
+      render :new
+    end
   end
   
   
   
   private
   def exercise_params
-    params.require(:exercise).permit(:body_region, :exercise)
+    params.require(:exercise).permit(:body_id, :name)
   end
   
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,6 @@
 class Exercise < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  
   has_many :workouts
   belongs_to :body
 end
