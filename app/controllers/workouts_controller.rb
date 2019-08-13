ef7298e10521a1c29e121928c73a2ba53343b5e6
Yuuki Fujii <brave7822@gmail.com>
2019-08-13 01:43:57 +0000

    workout only add

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,9 +1,26 @@
 class WorkoutsController < ApplicationController
   def new
     @workout = Workout.new
+    @menu = Menu.new
   end
   
   def create
+    @workout = Workout.new(workout_params)
+    @workout.user_id = current_user.id
+    
+    if @workout.save
+      redirect_to workouts_new_path, success: 'ワークアウトを記録しました'
+    else
+      flash.now[:danger] = "ワークアウトの記録に失敗しました"
+      render :new
+    end
   end
+  
+  private
+  def workout_params
+    params.require(:workout).permit(:date, :exercise_id, menus_attributes: [:set_count, :weight, :rep, :note])
+  end
+  
+  
   
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,6 @@ class Workout < ApplicationRecord
   has_many :menus
   belongs_to :user
   belongs_to :exercise
+  
+  accepts_nested_attributes_for :menus
 end
